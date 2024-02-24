9e161721297b52b790e9d1a9778d4cbf3fd7097c
lvalvarez1 <leandrov.alvarez2@gmail.com>
2024-02-24 14:47:47 -0300

    comercio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import CartWidget from "./CartWidget";
+
+
+const Navbar = () => {
+    return (
+        <nav className="bg-gray-700">
+            <div><span className="text-white font-bold pl-4 ">Comercio Reactivo</span></div>
+            <div class="flex justify-end list-none gap-6 pr-20">
+                <div><li className="text-white font-black"><a href="#">Inicio</a></li></div>
+                <div><li className="text-white font-black"><a href="#">Producto</a></li></div>
+                <div><li className="text-white font-black"><a href="#">Contacto</a></li></div>        
+                     
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
